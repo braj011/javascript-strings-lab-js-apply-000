40dcb8ed829d04b118180fd8f312150078cbbf26
braj011 <bhagyaraj01@gmail.com>
2018-07-23 15:25:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
+
+
+/*
+it('defines `greeting`', function() {
+  expect(greeting).toEqual("Hello, everybody!")
+})        */
 
 var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
+
+/*  NEXT ONE TO FIX
+
+it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
+  expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
+})    
+
+ERROR:
+concatenates strings to greet a special guest in `greetSpecialGuest`:
+
+      Error: Expected 'Neil deGrasse Tyson!' to equal 'Hello, Neil deGrasse Tyson!'
+      + expected - actual
+
+      -Neil deGrasse Tyson!
+      +Hello, Neil deGrasse Tyson!
+
+
+*/ 
 
 var topic = "space";
 
